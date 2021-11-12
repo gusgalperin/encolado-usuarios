3625c74a5f237207845d42abf3399a4a3d24fa38
gusgalperin <gusgalper@gmail.com>
2021-11-12 09:06:43 -0300

    fix main

--- src/main.js
+++ src/main.js
@@ -9,22 +9,14 @@ try {
     const puerto = await server.conectar()
     console.log(`conectado en el puerto ${puerto}`)
 
-    let clienteMongo;
-
     if(USE_MONGO){
-        clienteMongo = Cliente.getCliente()
+        const clienteMongo = Cliente.getCliente()
         await clienteMongo.conectar()
         console.log('conectado a mongo')
 
         const dao = getDao()
         await dao.crearIndices();
     }
-    
 } catch (error) {
     console.log('ups, hubo un error...')
-} finally {
-    server.desconectar()
-
-    if(USE_MONGO)
-        await clienteMongo.desconectar()
 }
