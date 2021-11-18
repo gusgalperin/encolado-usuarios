0dd0858a26cf66db5b7af0d1891e3f1465709aef
gusgalperin <gusgalper@gmail.com>
2021-11-18 18:32:26 -0300

    auto en encoladoprovider

--- src/utils/moduloMail/encoladoProvider.js
+++ src/utils/moduloMail/encoladoProvider.js
@@ -1,5 +1,10 @@
 import SubjectAndBodyProvider from './subjectAndBodyProvider.js'
 
+/*
+* Autor: Galperin Gustavo
+*/
+
+
 class EncoladoProvider extends SubjectAndBodyProvider {
 
     constructor(datos){
